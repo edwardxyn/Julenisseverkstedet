b2e7732c7041ff7976d0bafd17606838eeef375f
Edward Dynestøl <edward.dynestl@gmail.com>
2021-12-07 10:00:36 +0100

    Added Background color

---
+++ Julenissensverksted-master/js/index.js
@@ -0,0 +1,7 @@
+function changeBackground(color) {
+    document.body.style.background = color;
+ }
+
+ window.addEventListener("load",function() { changeBackground('#DDA0DD') });
+
+ //kilde som hjalp meg code riktig js. : https://stackoverflow.com/questions/197748/how-do-i-change-the-background-color-with-javascript/19145226
